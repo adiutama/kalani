bafe6da6f5605168d0fdfb32ccfb9b92c710908b
Adi Utama <neogatotkaca@gmail.com>
2021-07-19 01:00:29 +0700

    feat: add tokopedia link and fix homepage style

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -12,8 +12,8 @@ const showProducts = () => {
 <template>
   <main class="flex justify-center">
     <div class="w-full lg:w-md">
-      <section class="h-full p-6">
-        <header class="flex flex-col w-full h-screen items-center justify-center">
+      <section class="h-full">
+        <header class="flex flex-col w-full h-screen items-center justify-center p-6">
           <BrandLogo size="120px" />
 
           <nav class="w-full mt-6">
@@ -25,38 +25,50 @@ const showProducts = () => {
               </li>
               <li class="my-4">
                 <a
-                  href="https://wa.me/6281259538840?text=Hai+Kalani.+Mau+tanya-tanya+nih."
-                  target="_blank"
-                  class="btn w-full"
-                >
-                  <mdi:whatsapp />
-                  <span class="ml-2" v-text="t('contact_us')" />
-                </a>
-              </li>
-              <li class="my-4">
-                <a
-                  href="https://www.instagram.com/eatkalani/"
-                  target="_blank"
-                  class="btn w-full"
-                >
-                  <mdi:instagram />
-                  <span class="ml-2" v-text="'Instagram'" />
-                </a>
-              </li>
-              <li class="my-4">
-                <a
                   href="/testimonial"
-                  class="btn w-full"
+                  class="btn flex-1"
                 >
                   <mdi:comment />
                   <span class="ml-2" v-text="t('testimonial')" />
                 </a>
               </li>
+              <li class="my-4">
+                <a
+                  href="https://wa.me/6281259538840?text=Hai+Kalani.+Mau+tanya-tanya+nih."
+                  target="_blank"
+                  class="btn flex-1"
+                >
+                  <mdi:whatsapp />
+                  <span class="ml-2" v-text="t('contact_us')" />
+                </a>
+              </li>
             </ul>
+
+            <div class="flex flex-wrap flex-row justify-center mb-4 mt-8">
+              <div class="w-full mb-4 text-center">
+                <span v-text="t('visit_also')" />
+              </div>
+              <a
+                href="https://www.instagram.com/eatkalani/"
+                target="_blank"
+                class="btn p-2 mx-2"
+              >
+                <img src="/images/brand/instagram.png" alt="Instagram" class="h-6">
+              </a>
+              <a
+                href="https://www.tokopedia.com/eatkalani/"
+                target="_blank"
+                class="btn p-2 mx-2"
+              >
+                <img src="/images/brand/tokopedia.png" alt="Tokopedia" class="h-6">
+              </a>
+            </div>
           </nav>
         </header>
 
-        <router-view />
+        <div class="p-6">
+          <router-view />
+        </div>
       </section>
     </div>
   </main>
